83171691a902441ca3fd18247560888fadb909d3
Hongzhi Song <dldz2002@gmail.com>
2013-11-30 00:41:50 +0800

    Should work but have syntax error. But time to sleep

--- p2.hs
+++ p2.hs
@@ -4,3 +4,7 @@ isPrime primeList x = ( all (\y -> x `mod` y /= 0) factor_candi)
 
 primeList :: [Integer]
 primeList = 2: filter (\x -> isPrime primeList x) [3,5..]
+
+maxPrimeFactor :: Integer -> Integer
+maxPrimeFactor x = if (sqrt.fromIntegral $ x) <= min_prime then min_prime else maxPrimeFactor $ x / min_prime
+    where min_prime = head $ dropWhile (\prime -> x `mod` prime /= 0) primeList
